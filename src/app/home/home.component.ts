7848f8b9eb7ead30068b5b6bfb60e323e2350033
David <nanotrejo@usal.es>
2021-08-03 19:23:17 +0200

    Feat (Component): Add navbar for home
    nanotrejoweb > NW-001

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+const typewriter = require('t-writer.js');
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const target = document.querySelector('.typewriter');
+   
+    const writer = new typewriter(target, {
+      loop: true,
+      typeSpeed: 100,
+      deleteSpeed: 100,
+      typeColor: '#00df9a',
+      cursorColor: 'rgba(255,255,255, 0.74)'
+    })
+      .type('entusiasta.')
+      .rest(500)
+      .changeOps({ deleteSpeed: 100 })
+      .remove(11)
+      .type('trabajador.')
+      .rest(500)
+      .remove(11)
+      .type('profesional.')
+      .rest(500)
+      .changeOps({ deleteSpeed: 100 })
+      .remove(12)
+      .rest(500)
+      // .type('change options on the go')
+      // .rest(500)
+      .clear()
+      .start()
+    }
+
+}
