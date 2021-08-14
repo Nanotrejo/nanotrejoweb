ea800a2ca78169822d7e67f51deb32b6fcb75ce0
David <nanotrejo@usal.es>
2021-08-14 21:45:59 +0200

    Code (About): Add service for about component
    nanotrejoweb > NW-001

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import localeES from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,14 +12,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { CardFlipModule } from './shared/card/card-flip/card-flip/card-flip.module';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { ProjectComponent } from './shared/project/project/project.component';
 import { FooterComponent } from './shared/footer/footer/footer.component';
 import { AboutComponent } from './shared/about/about/about.component';
 import { ContactComponent } from './shared/contact/contact/contact.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { TimelineComponent } from './shared/timeline/timeline/timeline.component';
+
+registerLocaleData(localeES, 'es');
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,6 +32,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     FooterComponent,
     AboutComponent,
     ContactComponent,
+    TimelineComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,10 +46,9 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     FormsModule,
     HttpClientModule,
     MatSnackBarModule,
-    
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'es'}],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
